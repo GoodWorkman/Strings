0e804d9d6d9e3eada3e81753b8bf2a7a0b8fb592
Nikaragis <nikolaj@iMac-Nikolaj.local>
2021-12-30 15:25:39 +0300

    Strings Homework

---
+++ String/Program.cs
@@ -0,0 +1,85 @@
+﻿/*Разработать статический класс Message, содержащий следующие статические методы для обработки текста:
+а) Вывести только те слова сообщения,  которые содержат не более n букв.
+б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+в) Найти самое длинное слово сообщения.
+г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+ * 
+ * 
+ */
+
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Message.message = "Это обычное сообщение которое ничего не значит и не имеет запятых";
+    }
+}
+
+public static class Message
+{
+    public static string message = "";
+
+    public static void PrintModifiedMessage(int n)
+    {
+        string[] words = message.Split(" ");
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Length <= n)
+            {
+                Console.Write(words[i] + " ");
+            }
+        }
+    }
+
+    public static void PrintWordsWithoutLastSign(char sign)
+    {
+        string[] words = message.Split(" ");
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i][words[i].Length - 1] != sign)
+            {
+                Console.Write(words[i] + " ");
+            }
+        }
+    }
+
+    public static void PrintLongestWord()
+    {
+        string[] words = message.Split(" ");
+        string result = words[0];
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (result.Length < words[i].Length)
+            {
+                result = words[i];
+            }
+        }
+        Console.Write(result);
+    }
+
+    public static void PrintLongestWords()
+    {
+        string[] words = message.Split(" ");
+        string result = words[0];
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (result.Length < words[i].Length)
+            {
+                result = words[i];
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder(result + " " + result);
+        Console.Write(result);
+    }
+}
+
+
---
+++ String2/Program.cs
@@ -0,0 +1,59 @@
+﻿/*Создать программу, которая будет проверять корректность ввода логина. 
+ * Корректным логином будет строка от 2 до 10 символов, содержащая только буквы латинского алфавита или цифры,
+ * при этом цифра не может быть первой:
+а) без использования регулярных выражений;
+ * 
+ */
+
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solve();
+    }
+
+    static void Solve()
+    {
+        string password = Console.ReadLine();
+        Console.WriteLine(IsCorrectPassword(password) ? "Password is correct!" : "Password is not correct!");
+    }
+
+    static bool IsCorrectPassword(string password)
+    {
+        if (password.Length < 2 || password.Length > 9) return false;
+        if (char.IsDigit(password[0])) return false;
+
+        foreach (char w in password)
+        {
+            if (!IsEnglishWord(w) && !char.IsDigit(w))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static bool IsEnglishWord(char word)
+    {
+        char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        bool result = false;
+        foreach (char w in alphabet)
+        {
+            if (w == char.ToUpper(word))
+            {
+                result = true;
+            }
+        }
+        return result;
+
+    }
+}
+
+
+
